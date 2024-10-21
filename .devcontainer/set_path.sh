5de3d8afd996bd8ace092ffe5e4a6a253e445b77
Gaurav Saini <147703805+gauravsaini04@users.noreply.github.com>
2024-10-21 17:17:26 +0530

    Update set_path.sh

--- .devcontainer/set_path.sh
+++ .devcontainer/set_path.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export PATH="/usr/local/share/nvm/current/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/${USERNAME}/.local/bin:$PATH"
+export PATH="/usr/local/share/nvm/current/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/${USER}/.local/bin:$PATH"
